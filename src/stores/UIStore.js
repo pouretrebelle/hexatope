c0d2d29f5c4850d49aed425b84e1c7cc3e0d9187
Charlotte Dann <ask@pouretrebelle.com>
2017-07-16 20:23:09 +0100

    :construction: make store for window size and mouse position

---
+++ src/stores/UIStore.js
@@ -0,0 +1,42 @@
+import { observable, action } from 'mobx';
+
+class UIStore {
+
+  @observable windowWidth = 0;
+  @observable windowHeight = 0;
+  @observable mouseX = 0;
+  @observable mouseY = 0;
+
+  constructor() {
+    if (typeof window !== 'undefined') {
+      this.createListeners();
+      this.updateDimensions();
+    }
+  }
+
+  createListeners() {
+    window.addEventListener('resize', this.onWindowResized);
+    window.addEventListener('mousemove', this.onMouseMoved);
+  }
+
+  @action
+  updateDimensions = () => {
+    if (this.resizeTimer) {
+      clearTimeout(this.resizeTimer);
+      this.resizeTimer = undefined;
+    }
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+  }
+
+  @action
+  updateMousePosition = (e) => {
+    this.mouseX = e.pageX;
+    this.mouseY = e.pageY;
+  }
+
+  onWindowResized = () => this.updateDimensions();
+  onMouseMoved = (e) => this.updateMousePosition(e);
+}
+
+export default UIStore;
