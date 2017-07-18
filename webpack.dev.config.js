964b5394e896ef7f2a0144037651fe7e5cb6b251
Charlotte Dann <ask@pouretrebelle.com>
2017-07-18 09:32:22 +0100

    :bug: Use .babelrc for Babel config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -34,9 +34,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          'presets': ['react', 'es2015', 'stage-0', 'react-hmre'],
-        },
       },
       {
         test: /\.json?$/,
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -38,9 +38,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          'presets': ['react', 'es2015', 'stage-0'],
-        },
       },
       {
         test: /\.json?$/,
