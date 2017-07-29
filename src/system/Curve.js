6b7385e8424d73b872ab7d2aaeb7a4d3cc1b92ef
Charlotte Dann <ask@pouretrebelle.com>
2017-07-29 22:39:30 +0100

    :construction: Assign pairs to Curves

--- src/system/Curve.js
+++ src/system/Curve.js
@@ -1,9 +1,19 @@
 class Curve {
-  constructor({ pos1, pos1Control, pos2Control, pos2 }) {
+  constructor({ pos1, pos1Control, pos2Control, pos2, joinType }) {
     this.pos1 = pos1;
     this.pos1Control = pos1Control;
     this.pos2Control = pos2Control;
     this.pos2 = pos2;
+
+    this.divergeAtPos1 = joinType === 1;
+    this.divergeAtPos2 = joinType === 2;
+
+    this.hasPair = !!joinType; // if joinType is 0 then it's a single line
+    this.pair = undefined;
+  }
+
+  assignPair(curve) {
+    this.pair = curve;
   }
 }
 
