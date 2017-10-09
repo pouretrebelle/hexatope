7629d467be2496c70d79bba3b66bf85d86ac111f
Charlotte Dann <ask@pouretrebelle.com>
2017-10-09 21:54:37 +0100

    :sparkles: Refine and add more options to Tooltip

--- src/components/common/Tooltip.js
+++ src/components/common/Tooltip.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import styles from './Tooltip.sass';
 
-const Tooltip = ({ label, children }) => (
-  <div className={styles.tooltipWrapper}>
-    <div className={styles.tooltip}>
-      {label}
+const Tooltip = ({ direction, label, children }) => {
+  const tooltipClasses = classNames({
+    [styles.tooltip]: true,
+    [styles.tooltipUp]: direction == 'up',
+    [styles.tooltipDown]: direction == 'down',
+  });
+
+  return (
+    <div className={styles.tooltipWrapper} tabIndex={0}>
+      <div className={tooltipClasses}>
+        {label}
+      </div>
+      {children}
     </div>
-    {children}
-  </div>
-);
+  );
+};
 
 Tooltip.propTypes = {
+  direction: PropTypes.string,
   label: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
+
+Tooltip.defaultProps = {
+  direction: 'up',
+}
 
 export default Tooltip;
