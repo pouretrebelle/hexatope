9024b752f7f2d30ef5ce4b3b13f6dac89a27849a
Charlotte Dann <ask@pouretrebelle.com>
2017-10-16 19:37:39 +0100

    :sparkles: Add Kickstarter logo and tweak Tooltip style for mobile

--- src/components/common/Tooltip.js
+++ src/components/common/Tooltip.js
@@ -4,11 +4,12 @@ import classNames from 'classnames';
 
 import styles from './Tooltip.sass';
 
-const Tooltip = ({ direction, label, children }) => {
+const Tooltip = ({ direction, mobileTilt, label, children }) => {
   const tooltipClasses = classNames({
     [styles.tooltip]: true,
     [styles.tooltipUp]: direction == 'up',
     [styles.tooltipDown]: direction == 'down',
+    [styles.tooltipTiltMobileLeft]: mobileTilt === 'left',
   });
 
   return (
@@ -23,6 +24,7 @@ const Tooltip = ({ direction, label, children }) => {
 
 Tooltip.propTypes = {
   direction: PropTypes.string,
+  mobileTilt: PropTypes.string,
   label: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
---
+++ src/constants/urls.js
@@ -0,0 +1,1 @@
+export const KICKSTARTER_URL = 'https://www.kickstarter.com/projects/1560307803/765558989?ref=4judvg&token=846afe13';
