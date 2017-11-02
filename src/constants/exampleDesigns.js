9e7b3c5a77288a2944ee17c1740c1fefcb15cf44
Charlotte Dann <ask@pouretrebelle.com>
2017-11-02 22:40:42 +0000

    :lipstick: Add example designs

--- src/constants/exampleDesigns.js
+++ src/constants/exampleDesigns.js
@@ -1,3 +1,106 @@
 export default [
-  [[false, false, false, [1, 0.607], false, [1, 0.249], false, [1, 0.432], false, false, false], [false, [1, 0.195], [1, 0.014], [1, 0.052], [1, 0.31], [1, 0.661], [1, 0.618], [1, 0.105], [1, 0.065], [1, 0.402], false], [false, false, [1, 0.637], [1, 0.216], [1, 0.684], [1, 0.799], [1, 0.338], [1, 0.561], [1, 0.594], false, false]],
+  // drop star with cross
+  [[false,[1,0.866],false,[1,0.434],[1,0.603],[1,0.456],false,[1,0.413],false],[false,[1,0.441],[1,0.841],[1,0.879],[2,0.695],[1,0.868],[1,0.825],[1,0.107],false],[false,false,false,false,[1,0.712],false,false,false,false]],
+
+  // drop star with petals
+  [[false,[2,0.866],false,[1,0.434],[1,0.603],[1,0.456],false,[2,0.413],false],[false,[2,0.441],[1,0.841],[1,0.879],[1,0.956],[1,0.868],[1,0.825],[2,0.107],false],[false,false,false,false,[2,0.712],false,false,false,false]],
+
+  // bean ribbon
+  [[false,[1,0.343],false,[1,0.02],false,false,false,false,false],[[1,0.372],false,[1,0.731],[1,0.135],[1,0.761],[1,0.651],[1,0.155],[1,0.895],false],[false,false,false,false,false,false,[1,0.993],false,false]],
+
+  // half a flower
+  [[false,[1,0.59],false,[2,0.649],false,[1,0.332],false],[[1,0.002],false,[1,0.957],[1,0.71],[1,0.862],false,[1,0.893]]],
+
+  // aztec drop
+  [[false,[1,0.935],false,[1,0.035],[2,0.578],[1,0.09],false,[1,0.891],false],[false,false,[1,0.336],[1,0.34],[1,0.898],[1,0.61],[1,0.334],false,false],[false,false,false,false,[2,0.311],false,false,false,false]],
+
+  // paw drop
+  [[false,[1,0.935],false,[1,0.275],[1,0.578],[1,0.33],false,[1,0.891],false],[false,false,[1,0.336],[2,0.044],[2,0.555],[2,0.313],[1,0.334],false,false],[false,false,false,false,[1,0.311],false,false,false,false]],
+
+  // giant drop
+  [[false,[1,0.015],false,[1,0.13],[1,0.751],[2,0.68],[1,0.912],[1,0.108],false,[1,0.872],false],[false,false,[1,0.398],[1,0.664],[1,0.411],[2,0.34],[1,0.333],[1,0.375],[1,0.605],false,false],[false,false,false,false,[1,0.431],false,[1,0.404],false,false,false,false]],
+
+  // big gradiated drop
+  [[false,[2,0.015],false,[1,0.337],[2,0.532],[2,0.555],[2,0.433],[1,0.314],false,[2,0.872],false],[false,false,[1,0.398],[1,0.664],[2,0.672],[1,0.601],[2,0.718],[1,0.375],[1,0.605],false,false],[false,false,false,false,[1,0.431],[1,0.466],[1,0.404],false,false,false,false]],
+
+  // undulating diamond
+  [[false,false,false,[1,0.609],false,false,false],[false,[1,0.393],[1,0.532],[2,0.555],[1,0.433],[1,0.666],false],[[1,0.659],[1,0.664],[1,0.672],[2,0.601],[1,0.51],[1,0.375],[1,0.866]],[false,false,[1,0.431],[1,0.466],[1,0.404],false,false]],
+
+  // fidget spinner flower
+  [[false,false,[1,0],[1,0.38],[2,0.657],[1,0.206],[1,0.052],false,false],[[1,0.88],[2,0.652],[1,0.384],[1,0.395],[2,0.406],[1,0.275],[1,0.234],[2,0.612],[1,0.512]],[false,false,[1,0.874],false,false,false,[1,0.695],false,false]],
+
+  // holey hexagon
+  [[false,false,false,false,false,[1,0.406],false,[1,0.554],false,false,false,false,false],[false,[1,0.678],[1,0.566],false,[1,0.225],[1,0.38],[1,0.532],[1,0.413],[1,0.277],false,[1,0.881],[1,0.76],false],[false,[1,0.442],[1,0.105],[1,0.736],[1,0.591],[1,0.395],[1,0.406],[1,0.482],[1,0.234],[1,0.695],[1,0.293],[1,0.628],false],[false,false,[1,0.35],false,[1,0.098],[1,0.37],false,[1,0.386],[1,0.047],false,[1,0.325],false,false]],
+
+  // soft giant hexagon
+  [[false,false,false,false,false,[1,0.667],false,[2,0.554],false,false,false,false,false],[false,[1,0.678],[2,0.566],[1,0.514],[1,0.051],[1,0.38],[1,0.532],[1,0.413],[1,0.277],[1,0.341],[1,0.662],[2,0.76],false],[false,[2,0.442],[1,0.311],[1,0.569],[1,0.591],[1,0.437],[1,0.406],[1,0.482],[1,0.504],[1,0.695],[1,0.293],[1,0.628],false],[false,false,[1,0.35],false,[1,0.305],[2,0.37],[1,0.714],[1,0.386],[1,0.047],false,[2,0.325],false,false]],
+
+  // small circular triangle
+  [[false,[1,0.442],[1,0.428],[2,0.726],[1,0.583],[2,0.526],[1,0.684],[1,0.506],false],[false,false,[1,0.526],[1,0.295],[2,0.733],[1,0.805],[1,0.632],false,false]],
+
+  // mobius triangle
+  [[[1,0.982],[1,0.442],[1,0.428],[2,0.257],[1,0.583],[2,0.349],[1,0.387],[1,0.506],[1,0.151]],[false,false,[1,0.526],[1,0.647],[2,0.264],[1,0.509],[1,0.632],false,false],[false,false,false,false,[1,0.664],false,false,false,false]],
+
+  // bubbly hexagon
+  [[false,[1,0.161],[1,0.977],[1,0.633],[1,0.878],[1,0.024],[1,0.908],[1,0.202],false],[[1,0.844],[1,0.241],[1,0.157],[1,0.511],[1,0.372],[1,0.038],[1,0.679],[1,0.175],[1,0.316]],[false,false,[1,0.931],false,[1,0.298],false,[1,0.73],false,false]],
+
+  // small art deco
+  [[false,[1,0.069],[1,0.69],[1,0.03],false],[[1,0.532],[1,0.616],[1,0.07],[1,0.645],[1,0.6]],[false,false,[2,0.971],false,false]],
+
+  // bubbly deco diamond
+  [[false,false,false,[1,0.647],false,[1,0.261],[1,0.603],[1,0.283],false,[1,0.455],false,false,false],[[1,0.429],false,[1,0.164],[1,0.441],[1,0.841],[1,0.292],[2,0.955],[1,0.281],[1,0.825],[1,0.367],[1,0.218],false,[1,0.786]],[false,false,false,false,false,false,[1,0.712],false,false,false,false,false,false]],
+
+  // deco loop
+  [[false,[1,0.356],[1,0.051],[1,0.576],[1,0.219],[1,0.814],false],[[1,0.458],[1,0.717],false,[1,0.522],false,[1,0.743],[1,0.746]],[false,false,[1,0.046],false,[1,0.225],false,false]],
+
+  // diamond from circles
+  [[false,false,false,[1,0.168],false,[1,0.177],false,false,false],[false,[1,0.342],[1,0.456],[1,0.482],[1,0.385],[1,0.406],[1,0.487],[1,0.035],false],[false,false,[1,0.544],[1,0.057],[1,0.481],[1,0.202],[1,0.663],false,false]],
+
+  // cute fountain swish
+  [[false,false,false,[1,0.482],false,[1,0.36],false,[2,0.638],false,false,false],[[1,0.381],false,[1,0.427],[1,0.601],[2,0.02],false,[1,0.005],[2,0.604],[1,0.766],false,[1,0.575]]],
+
+  // l system
+  [[false,[1,0.124],false,false,[1,0.858],[1,0.131],[1,0.561],[1,0.575],false],[[1,0.255],[1,0.396],[1,0.048],[1,0.028],[1,0.263],false,[1,0.105],[1,0.184],[1,0.68]],[false,false,[1,0.514],false,[1,0.51],false,false,false,false]],
+
+  // symmetry star
+  [[false,false,false,[1,0.193],false,false,false,false,false,[1,0.6],false,false,false],[false,false,false,[1,0.392],[1,0.094],[1,0.133],[1,0.416],[1,0.336],[1,0.166],[1,0.367],false,false,false],[[1,0.317],false,[1,0.078],[1,0.451],[1,0.392],[1,0.054],false,[1,0.302],[1,0.134],[1,0.547],[1,0.28],false,[1,0.422]],[false,false,false,[1,0.568],[1,0.216],false,[1,0.446],false,[1,0.325],[1,0.955],false,false,false]],
+
+  // ribbony star
+  [[false,false,false,[1,0.193],false,false,false,false,false,[1,0.6],false,false,false],[false,false,false,[1,0.392],[1,0.094],[2,0.737],[1,0.416],[1,0.336],[1,0.166],[1,0.367],false,false,false],[[1,0.317],false,[1,0.078],[1,0.451],[1,0.392],[2,0.658],[1,0.458],[1,0.302],[2,0.654],[1,0.547],[1,0.28],false,[1,0.422]],[false,false,false,[1,0.568],[1,0.216],false,[1,0.446],false,[1,0.325],[1,0.955],false,false,false]],
+
+  // whispy propeller
+  [[false,[1,0.286],[1,0.771],[1,0.405],false,[1,0.251],false],[false,[1,0.046],[1,0.401],[1,0.986],[1,0.573],false,[1,0.172]]],
+
+  // propeller
+  [[false,[2,0.547],false,[1,0.611],[1,0.15],[2,0.814],false],[[1,0.551],false,[1,0.608],[2,0.517],[1,0.78],[1,0.445],false]],
+
+  // flicky propeller
+  [[[1,0.678],false,[1,0.471],[1,0.833],[2,0.608],[1,0.86],false,[1,0.719],[1,0.385]],[false,false,[2,0.707],[1,0.311],[1,0.941],false,[2,0.414],false,false],[false,false,false,false,[1,0.509],false,false,false,false]],
+
+  // twisty triangle
+  [[false,false,false,false,false,[1,0.518],false,[1,0.649],false],[[1,0.134],[1,0.586],[1,0.341],[2,0.962],[2,0.939],[2,0.96],[1,0.336],false,false],[false,false,false,false,[1,0.583],[1,0.167],[1,0.987],false,false]],
+
+  // blob with hole
+  [[false,false,false,[1,0.168],false,[1,0.647],false,false,false],[false,[1,0.332],[1,0.34],false,[1,0.477],[2,0.265],[1,0.183],[1,0.348],[1,0.046]],[false,false,[1,0.07],false,[1,0.425],false,[1,0.967],false,false]],
+
+  // circle with half moon
+  [[false,[1,0.729],false,[1,0.821],false],[[1,0.532],[2,0.557],false,[2,0.523],[1,0.737]]],
+
+  // small triangle with circle hole
+  [[false,[2,0.128],false,[1,0.052],false,[1,0.182],false,[2,0.219],false],[false,false,[1,0.074],[1,0.052],false,[1,0.147],[1,0.034],false,false],[false,false,false,false,[2,0.487],false,false,false,false]],
+
+  // headress
+  [[false,[2,0.781],false,[1,0.161],[1,0.024],[1,0.199],false,[2,0.877],false],[false,false,[1,0.068],[1,0.145],false,[1,0.076],[1,0.007],false,false]],
+
+  // deco drop
+  [[false,false,false,[1,0.224],[1,0.031],[1,0.081],false,false,false],[false,[1,0.043],[1,0.357],[1,0.944],[2,0.558],[1,0.869],[1,0.571],[1,0.979],false],[false,false,false,false,[1,0.01],false,false,false,false]],
+
+  // big elegant drop
+  [[false,[1,0.177],false,[1,0.807],false,false,false],[[1,0.416],[2,0.503],false,[1,0.106],[2,0.659],[1,0.567],[1,0.573]],[false,false,[1,0.564],[1,0.276],[1,0.694],[2,0.667],false],[false,[1,0.313],[1,0.553],[2,0.535],[2,0.79],false,false]],
+
+  // big flourish
+  [[false,[1,0.733],[1,0.341],false,false,[2,0.669],false,false,false,false,false],[false,false,[1,0.022],false,[1,0.919],[2,0.024],[1,0.623],false,[1,0.649],[1,0.934],false],[false,false,false,false,false,false,false,false,[1,0.138],false,false]],
+
+  // celtic flower with center star
+  [[false,[1,0.516],[1,0.347],[1,0.481],[1,0.617],[1,0.937],[1,0.311],[1,0.407],false],[[1,0.327],[1,0.502],[1,0.671],[1,0.897],false,[1,0.529],[1,0.735],[1,0.496],[1,0.742]],[false,false,[1,0.415],false,[1,0.445],false,[1,0.758],false,false]],
 ];
