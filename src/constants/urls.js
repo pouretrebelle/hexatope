bdb68bcc794825f6028930cebde7b88f3971a06b
Charlotte Dann <ask@pouretrebelle.com>
2017-11-17 00:06:51 +0000

    :sparkles: Add social logos to header

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,4 @@
 export const KICKSTARTER_URL = 'https://www.kickstarter.com/projects/charlottedann/hexatope-design-your-own-unique-jewellery';
+export const TWITTER_URL = 'https://twitter.com/hexatope';
+export const INSTAGRAM_URL = 'https://www.instagram.com/hexatope';
+export const FACEBOOK_URL = 'https://www.facebook.com/hexatope';
